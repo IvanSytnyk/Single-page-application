bbc1fd7afdab55c5615622e47912ee97c5ccc461
Ivan Sytnyk <uaivan.sytnyk@gmail.com>
2023-02-22 22:57:24 +0200

    Added loader.js to process async functions and show loader before they will be fulfilled

---
+++ loader.js
@@ -0,0 +1,21 @@
+export const asyncProvider = async (func) => {
+ 
+    let body = document.querySelector('body');
+ 
+    let loadingElement = document.createElement('loading');
+ 
+    loadingElement.innerHTML = 'Loading movies';
+ 
+    body.append(loadingElement);
+ 
+    try {
+        if (typeof func === "function") {
+ 
+            return await func();
+        }
+    } catch (error) {
+        console.log("Error in provider:", { error });
+    } finally {
+        loadingElement.remove();
+    }
+};
