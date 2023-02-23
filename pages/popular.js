a943b7ce8bf89a779eea9149aa76ccc251c70f14
Ivan Sytnyk <uaivan.sytnyk@gmail.com>
2023-02-23 16:48:39 +0200

    Added search input and style it

--- pages/popular.js
+++ pages/popular.js
@@ -30,9 +30,16 @@ export const Films = (isLiked) => {
             <header>
                 <div class = "head">
                   <i class="fa-solid fa-film fa-2xl"></i>
-                  <h1 id="heading">${isLiked ? "Bookmarks" : " TheMovieDB PoC"}</h1>
+                  <h1 id="heading">${
+                    isLiked ? "Bookmarks" : " TheMovieDB PoC"
+                  }</h1>
                 </div>
-                ${!isLiked ? '<button id="books">Bookmarks</button>' : "" }
+                ${!isLiked ? '<button id="books">Bookmarks</button>' : ""}
+                <div class="search">
+                  <svg id="svg" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
+                    <input class="input" type="text" placeholder="Search">
+                  </input>
+                </div>
             </header>
             <div class="list" id="list">
               <ul>
